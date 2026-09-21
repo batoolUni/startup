b6d57cb002240c4ce4f5b2b570505e8204c4c5dc
batoolUni <batool.university@gmail.com>
2026-09-21 20:51:20 +0300

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -3,8 +3,10 @@ import time
 import threading
 import numpy as np
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # ==========================================
 # 1. GLOBAL STATE & ATTACK SIMULATOR STATE
